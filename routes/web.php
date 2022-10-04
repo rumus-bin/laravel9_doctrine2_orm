b4a8b31e3ee6e185f849b545286f3b902bb98aa3
RumusBin <komlikov.r@gmail.com>
2022-10-04 18:05:54 +0100

    Auth install

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( '/users', [UserController::class, 'index']);
-Route::get( '/users/show/{user_id}', [UserController::class, 'show']);
-Route::post( '/users', [UserController::class, 'store']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
+require __DIR__.'/auth.php';
